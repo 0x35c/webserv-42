24e51478ddcddc1f39144d3e32d53ed2a50bf0c5
averdon <anthonyverdon10@gmail.com>
2023-04-25 13:47:53 +0200

    add a while(true) loop, and we can read and write to request

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sstream>
@@ -6,6 +7,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <string.h>
+#include <cerrno>
 /*
 struct in_addr{
 	unsigned long s_addr;
@@ -36,11 +38,14 @@ int	main()
 	socklen_t	socketAddress_len = sizeof(socketAddress);
 	socketAddress.sin_family = AF_INET;
 	socketAddress.sin_port = htons(PORT);
-	socketAddress.sin_addr.s_addr = inet_addr(IP);
+	socketAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	//bind to the PORT with the informations above
 	if (bind(socket_server, (sockaddr*)&socketAddress, socketAddress_len) < 0)
+	{
+		std::cout << strerror(errno) << "\n";
 		return (2);
+	}
 
 	//start to listen to request (= wait)
 	if (listen(socket_server, 1) < 0)
@@ -53,16 +58,31 @@ int	main()
 	std::cout << ss.str() << std::endl;
 
 	//when someone connect, it accept it
-	int client = accept(socket_server, (sockaddr*)&socketAddress, &socketAddress_len);
-	if (client < 0)
+	const int BUFFER_SIZE = 30720;
+	while (true)
 	{
-		std::ostringstream ss;
-		ss <<
-		"Server failed to accept incoming connection from ADDRESS: "
-		<< inet_ntoa(socketAddress.sin_addr) << "; PORT: "
-		<< ntohs(socketAddress.sin_port);
-		exit(0);
+		int client_socket = accept(socket_server, (sockaddr*)&socketAddress, &socketAddress_len);
+		if (client_socket < 0)
+		{
+			std::ostringstream ss;
+			ss <<
+			"Server failed to accept incoming connection from ADDRESS: "
+			<< inet_ntoa(socketAddress.sin_addr) << "; PORT: "
+			<< ntohs(socketAddress.sin_port);
+			return (4);
+		}
+		std::cout << "someone connected.\n";
+		char buffer[BUFFER_SIZE] = {0};
+		if (read(client_socket, buffer, BUFFER_SIZE) < 0)
+			return (5);
+		std::cout << buffer << "\n";
+		std::string htmlFile = "<!DOCTYPE html><html lang=\"en\"><body><h1> HOME </h1><p> Hello from your Server :) </p></body></html>";
+        std::ostringstream ss;
+        ss << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " << htmlFile.size() << "\n\n"
+           << htmlFile;
+		write(client_socket, ss.str().c_str(), ss.str().size());
+
+		close(client_socket);
 	}
-	std::cout << "someone connected.\n";
-	close(client);
+	close(socket_server);
 }

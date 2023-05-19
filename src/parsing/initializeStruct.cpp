b96a87b777e30eb7f8c9655db64f416dea86a962
averdon <anthonyverdon10@gmail.com>
2023-05-19 12:42:34 +0200

    no push

---
+++ src/parsing/initializeStruct.cpp
@@ -0,0 +1,25 @@
+#include "parsing.hpp"
+
+void	initializeLocation(t_location & location)
+{
+	location.locationPath = "";
+	for (int i = 0; i < 3; i++)
+		location.methodsAllowed[i] = false;
+	location.redirectionCode = 0;
+	location.redirectionPath = "";
+	location.root = "";
+	location.directoryListing = false;
+	location.index = "";
+	location.acceptUploadedFile = false;
+	location.uploadedFilePath = "";
+}
+
+void	initializeServer(t_server & server)
+{
+	server.host = "";
+	server.port = 0;
+	server.server_name = "";
+	server.isDefaultServer = false;
+	server.errpage = "";
+	server.maxFileSizeUpload = 0;
+}
